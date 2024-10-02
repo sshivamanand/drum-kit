df9bf5a8f259be466be34365949a20dc6164ff09
Shivam Anand <shivamanand1635@gmail.com>
2024-10-02 21:06:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 function playSound(key) {
   let sound;
+  buttonAnimation(key);
   switch (key) {
     case "w":
-      sound = new Audio("/sounds/tom-1.mp3");
+      sound = new Audio("sounds/tom-1.mp3");
       break;
     case "a":
-      sound = new Audio("/sounds/tom-2.mp3");
+      sound = new Audio("sounds/tom-2.mp3");
       break;
     case "s":
-      sound = new Audio("/sounds/tom-3.mp3");
+      sound = new Audio("sounds/tom-3.mp3");
       break;
     case "d":
-      sound = new Audio("/sounds/tom-4.mp3");
+      sound = new Audio("sounds/tom-4.mp3");
       break;
     case "j":
-      sound = new Audio("/sounds/crash.mp3");
+      sound = new Audio("sounds/crash.mp3");
       break;
     case "k":
-      sound = new Audio("/sounds/kick-bass.mp3");
+      sound = new Audio("sounds/kick-bass.mp3");
       break;
     case "l":
-      sound = new Audio("/sounds/snare.mp3");
+      sound = new Audio("sounds/snare.mp3");
       break;
     default:
       return;
@@ -59,3 +60,11 @@ document.querySelector(".k").addEventListener("click", function () {
 document.querySelector(".l").addEventListener("click", function () {
   playSound("l");
 });
+
+function buttonAnimation(key) {
+  var selectedButton = document.querySelector("." + key);
+  selectedButton.classList.add("pressed");
+  setTimeout(() => {
+    selectedButton.classList.remove("pressed");
+  }, 200);
+}
